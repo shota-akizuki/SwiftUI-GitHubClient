42c0090f19bbd467a80b112e07f374fe95feff60
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-05-15 14:22:36 +0900

    Initial Commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 秋月翔太 on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
